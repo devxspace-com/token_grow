2620b1b0ceb9d2ed6c47dac8c371597d16f413f7
vicsnet <adesanmiadebisi@gmail.com>
2023-06-21 14:07:59 +0100

    Notification Desktop Done

--- src/app/notification/page.tsx
+++ src/app/notification/page.tsx
@@ -1,9 +1,10 @@
 import Layout from "@/components/Layout";
+import NotificationFeatures from "./features/NotificationFeatures";
 
 export default function Notification() {
     return (
       <Layout>
-      <div>page</div>
+     <NotificationFeatures/>
       </Layout>
     )
   }
