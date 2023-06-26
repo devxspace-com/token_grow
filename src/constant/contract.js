b65aeefd6651da40ced6adedbdcfe7af23826b3e
vicsnet <adesanmiadebisi@gmail.com>
2023-06-26 15:30:25 +0100

    address changed

--- src/constant/contract.js
+++ src/constant/contract.js
@@ -1,10 +1,11 @@
 /** @format */
 
-export const TokenGrowAddr = "0x7d23ad726af604de5bd9cd2fba9030f6a45185b6";
+export const TokenGrowAddr = "0x4af5a075f16b75fadb288e6153d602e9d606901d";
 
 export const NFTAddr = "0x96766299D396CAe2b7f289f64908c85E81B2928b";
 
-export const Token = "0xba6844763ebc367b9867f744be024a857bc3658a";
+export const Token = "0xd415Dd020231345748f8D7699E34DF48bf263D2D";
+
 
 // Test Token: 0xba6844763ebc367b9867f744be024a857bc3658a
 
