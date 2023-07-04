c047a70f23446c4a4736ca9185fc8cc16e895ccf
vicsnet <adesanmiadebisi@gmail.com>
2023-07-04 01:29:47 +0100

    contract changed to mainnet

--- src/constant/contract.js
+++ src/constant/contract.js
@@ -1,11 +1,16 @@
 /** @format */
 
-export const TokenGrowAddr = "0x9fa12a7729964B15B3D5971bC87b758598e176f9";
+export const TokenGrowAddr = "0xD8AE2f85D83D108D8E3456E9E0bc97Ae3fe2dF5C";
 
-export const NFTAddr = "0xfb6948aBa275D242a8247dCb4B96181ceE016768";
+export const NFTAddr = "0x713DBBe861153BE4ebcDE75e423ee5343feA1478";
 
-export const Token = "0x929c485acdca59c805e9f76f715b04c5b6a29e92";
+export const Token = "0xb4EdAB6Da45ceA2E8C5116e49f4564649E699d3c";
 
+// Token mainnet: 
+
+// NFT mainnet: 
+
+// TokenGrow mainnet: 
 
 // Test Token: 0x929c485acdca59c805e9f76f715b04c5b6a29e92
 
